4f36b26cf6c23427a9186bedf8f06d1dbfc5d5c2
anastafj197 <frankie.anastasia@gmail.com>
2017-11-07 15:26:09 -0500

    Added functionality
    Scanner for quality of life 
    Attempted multi stops

--- speed.java
+++ speed.java
@@ -24,10 +24,53 @@ public class Speed {
   public static double newMilesPerMin;
 
   public static void main(String [] args) {
+ 
+    Scanner input = new Scanner(System.in);
 
-    Double distance = Double.valueOf(args[0]);
-    Double speedLimit = Double.valueOf(args[1]);
-    Double speedAmount = Double.valueOf(args[2]);
+    
+    System.out.println();
+    System.out.println("                   * Welcome to the SpeedSvaer Calculator *");
+    System.out.println();
+    System.out.println("               This app is primarily for long distance road trips");
+    System.out.println("                 Enter in the number of stops you will be making  ");
+    
+
+    int numStops = input.nextInt();
+
+    for (int n = 0; n < numStops; n++) {
+
+
+    }
+
+    
+    System.out.println("          Please enter the following 3 numbers seperated by a space"); 
+    System.out.println("                      For each stop you will be making");
+    System.out.println();
+    System.out.println("                 The DISTANCE in miles you wish to travel");
+    System.out.println("                    The SPEEDLIMIT of the road your on");
+    System.out.println("           The number of miles OVER / UNDER the speedLimit you wish");
+    
+    //String input = input.nextLine();
+    
+    double[] values = new double[3];
+
+    for (int i = 0; i < 3; i++) {
+      values[i] = input.nextDouble();
+    }
+
+    System.out.println("HERE");
+    
+    //String[] numbers = input.split(" ");
+    
+    System.out.println("HERE");
+    
+    Double distance = values[0];
+
+    Double speedLimit = values[1];
+
+    Double speedAmount = values[2];
+    
+    System.out.println("HERE");
 
     if (distance < 0 || speedLimit < 0) {
       System.out.println("No negative values here");
